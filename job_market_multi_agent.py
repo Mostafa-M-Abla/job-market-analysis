b61c1269507640d21ad910eab3128712e49def65
DESKTOP-17N28E2\mosta <mostafa.m.abla@gmail.com>
2026-01-12 13:38:56 +0200

    Task 3 working. Top skill already selected

--- job_market_multi_agent.py
+++ job_market_multi_agent.py
@@ -2,6 +2,7 @@ import warnings
 warnings.filterwarnings("ignore")
 import time
 from datetime import datetime
+from langchain_openai import ChatOpenAI
 
 import json
 from pathlib import Path
@@ -30,22 +31,26 @@ from google_jobs_tool import GoogleJobsCollectorTool
 OUTPUT_DIR = Path("outputs")
 OUTPUT_DIR.mkdir(exist_ok=True)
 
+
 load_dotenv()
 
-# Agent
+# Agent 1: Job Postings Finder
 job_agent = Agent(
     role="Job Postings Finder",
     goal="Find relevant job postings for the given job titles and country and return structured results.",
     backstory="You are an expert job market data collector using Google Jobs API.",
+    # llm=ChatOpenAI(model="gpt-4.5-turbo", temperature=0),
     tools=[GoogleJobsCollectorTool()],
+    allow_delegation=False,
     verbose=True,
 )
 
-# Task
+# Task 1: Find Job Postings
 job_search_task = Task(
     description=(
         "Use the Google Jobs Collector Tool to retrieve {total_num_posts} job postings for these titles: {job_titles} "
-        "in {country}. Ensure each posting has title/company/location and include full job description when available."
+        "in {country}, you can use one or more of the given job titles for your search. Ensure each posting has title/company/location and "
+        "include full job description when available. Make sure that the all retrieved job postings are very close to the given job titles."
     ),
     expected_output=(
         "A JSON-like list of job postings (length = {total_num_posts}). Each item must include: "
@@ -54,9 +59,73 @@ job_search_task = Task(
     agent=job_agent,
 )
 
+# Agent 2: Job Requirements Extractor
+requirements_extractor_agent = Agent(
+    role="Job Requirements Extractor",
+    goal="Extract technical skills, tools, cloud platforms, and required certifications from job descriptions into strict JSON schema.",
+    backstory="You are an expert Job Position analyzer, you can understand the needed requirements in a Job post. "
+              "You are precise and avoid hallucinating items not present in the text. And you avoid duplications",
+    verbose=True,
+    allow_delegation=False,
+    # llm=f"openai/{OPENAI_MODEL}",
+)
+
+# Task 2: Extract Job Requirements
+job_requirements_task = Task(
+    description=(
+        "Analyze the retrieved job postings and extract the required technical skills, tools, cloud platforms (only 3 "
+        "cloud platforms are allowed in that category: AWS 'Amazon Webs services' or GCP 'Google Cloud Platform' or Azure), "
+        "and certifications into a JSON array. Each entry should correspond to a job posting and include: "
+        "'job_id', 'technical_skills' (list), 'tools' (list), 'cloud_platforms' (list), 'certifications' (list). "
+        "Avoid duplications and only include items explicitly mentioned in the descriptions."
+    ),
+    expected_output=(
+        "A JSON-like list where each item has: job_id, technical_skills (list), tools (list), "
+        "cloud_platforms'AWS, Azure or GCP' (list), certifications (list)."
+    ),
+    agent=requirements_extractor_agent,
+)
+
+# Agent 3: Job Requirements Analyzer
+requirements_analyzer_agent = Agent(
+    role="Job Requirements Analyzer",
+    goal="Create an overview of most in demand skills and tools",
+    backstory="You are an expert Job requirements analyzer, you can understand and group job requirements, "
+              "you understand synonyms. e.g. ML and Machine Learning both point teh same skill"
+              "You are precise and avoid hallucinating items not present in the text. And you avoid duplications",
+    verbose=True,
+    allow_delegation=False,
+    # llm=f"openai/{OPENAI_MODEL}",
+)
+
+#Task 3: Analyze Job Requirements and create markdown Report
+job_requirements_analysis_task = Task(
+    description=(
+        "Given the extracted job requirements from multiple job postings, Analyze the extracted job requirements and "
+        "create a markdown report, providing teh following info:"
+        "1- Top 15 technical skills mentioned across all job postings."
+        "2- Top 15 tools mentioned across all job postings."
+        "3- Top 3 cloud platforms mentioned across all job postings (only 3 platforms are allowed in that category: AWS, Azure, GCP)."
+        "4- Top 5 certifications mentioned across all job postings."
+        "Note: If a tool or skill appears multiple times in a single job posting, count it only once for that posting."
+        "The output for each category (technical skills, tools, cloud platforms and certifications) should show a "
+        "a column mentioning number of positions where the item appeared and a column mentioning % of positions where item appeared."
+        "Avoid duplication of skills and avoid hallucinating items not present in the text."
+    ),
+    expected_output=(
+        "A markdown report containing the requested info and the report should be well formatted with proper headings and tables/Charts."
+    ),
+    agent=requirements_analyzer_agent,
+)
+
+
+#TODO tell tasks/agemts
+
+
+
 crew = Crew(
-    agents=[job_agent],
-    tasks=[job_search_task],
+    agents=[job_agent, requirements_extractor_agent, requirements_analyzer_agent],
+    tasks=[job_search_task, job_requirements_task, job_requirements_analysis_task],
     process=Process.sequential,
 )
 
@@ -67,7 +136,7 @@ if __name__ == "__main__":
     result = crew.kickoff(inputs={
         "job_titles": ["AI Engineer", "Generative AI Engineer", "GenAI Engineer", "Agentic AI Engineer", "Machine Learning Engineer", "ML Engineer"],
         "country": "Egypt",
-        "total_num_posts": 2,
+        "total_num_posts": 3,
     })
     print("\nFINAL OUTPUT:\n", result)
 
@@ -102,137 +171,4 @@ if __name__ == "__main__":
     print(f"Saved final result: {final_path.resolve()}")
 
 
-    print("Execution time in minutes = ",  (time.time() - start_time)/60)
-
-
-
-
-
-# # Agent 1:
-# job_agent = Agent(
-#     role="Job Postings Finder and Verifier",
-#     goal="Do amazing Job in searching the internet and find Job listings relevant to the search.",
-#     backstory=(
-#         "You are an expert in online job hunting. You know how to find job listings, "
-#         "extract and validate them, and ensure they match the specified job role."
-#     ),
-#     tools=[SerperDevTool(),DownloadPageTool()],
-#     verbose=True
-# )
-#
-# # Task
-# job_search_task = Task(
-#     description=(
-#         "Search Google for 20 job listings matching the title: {job_title}. "
-#         "For each URL, download the HTML using your tool. "
-#         "Analyze the content and confirm if it's a real job posting related to the title. "
-#         "Only keep listings that are valid and relevant. "
-#         "Save the valid HTML pages locally and return a summary list with:\n"
-#         "- URL\n"
-#         "- Page title (cleaned)\n"
-#         "- Local HTML file path"
-#     ),
-#     expected_output=(
-#         "A list of 20 validated job listings. Each entry must include:\n"
-#         "- The original job listing URL\n"
-#         "- The page title or identified job title\n"
-#         "- Path to the saved HTML file"
-#     ),
-#     agent=job_agent
-# )
-#
-# # Crew setup
-# crew = Crew(
-#     agents=[job_agent],
-#     tasks=[job_search_task],
-#     process=Process.sequential
-# )
-#
-# # "job_titles": "AI Engineer, Generative AI Engineer, GenAI Engineer, Agentic AI Engineer",
-#
-# # Run the crew
-# if __name__ == "__main__":
-#     result = crew.kickoff(inputs={
-#         "job_title": "AI Engineer",
-#         "country": "Egypt",
-#         "total_num_posts": 2,
-#     })
-#     print("\nFINAL OUTPUT:\n", result)
-#
-
-
-
-
-#
-# # Agent 1: Researcher
-# researcher = Agent(
-#     role="Job Postings Collector",
-#     goal="Make sure to do amazing analysis on "
-#          "job posting to help job applicants",
-#     tools = [scrape_tool, search_tool],
-#     verbose=True,
-#     backstory=(
-#         "As a Job Researcher, your prowess in "
-#         "navigating and extracting critical "
-#         "information from job postings is unmatched."
-#         "Your skills help pinpoint the necessary "
-#         "qualifications and skills sought "
-#         "by employers, forming the foundation for "
-#         "effective application tailoring."
-#     )
-# )
-#
-#
-# # Agent 2: Profiler
-# profiler = Agent(
-#     role="Personal Profiler for Engineers",
-#     goal="Do increditble research on job applicants "
-#          "to help them stand out in the job market",
-#     tools = [scrape_tool, search_tool,
-#              read_resume, semantic_search_resume],
-#     verbose=True,
-#     backstory=(
-#         "Equipped with analytical prowess, you dissect "
-#         "and synthesize information "
-#         "from diverse sources to craft comprehensive "
-#         "personal and professional profiles, laying the "
-#         "groundwork for personalized resume enhancements."
-#     )
-# )
-#
-#
-# # Task 1: Task for Researcher Agent: Extract Job Requirements
-# research_task = Task(
-#     description=(
-#         "Analyze the job posting URL provided ({job_posting_url}) "
-#         "to extract key skills, experiences, and qualifications "
-#         "required. Use the tools to gather content and identify "
-#         "and categorize the requirements."
-#     ),
-#     expected_output=(
-#         "A structured list of job requirements, including necessary "
-#         "skills, qualifications, and experiences."
-#     ),
-#     agent=researcher,
-#     async_execution=True
-# )
-#
-#
-#
-# # Task2: Task for Profiler Agent: Compile Comprehensive Profile
-# profile_task = Task(
-#     description=(
-#         "Compile a detailed personal and professional profile "
-#         "using the GitHub ({github_url}) URLs, and personal write-up "
-#         "({personal_writeup}). Utilize tools to extract and "
-#         "synthesize information from these sources."
-#     ),
-#     expected_output=(
-#         "A comprehensive profile document that includes skills, "
-#         "project experiences, contributions, interests, and "
-#         "communication style."
-#     ),
-#     agent=profiler,
-#     async_execution=True
-# )
-#
+    print("Execution time: ",  ((time.time() - start_time)/60), " minutes")
